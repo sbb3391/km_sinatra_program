d836094decd8b3733595c2dfe1e9e4f7f183da29
sbb3391 <sbb33391@gmail.com>
2021-02-15 14:23:53 -0500

    fix params in /prosals/new form to accept params hash to create new proposal & line items

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -15,7 +15,7 @@ class ProposalsController < Sinatra::Base
     erb :'proposals/new.html'
 
   end
-  
+
   get '/proposals/:id' do 
     @proposal = Proposal.find(params[:id])
     @categories = ["Engine", "Install", "Paper Supply", "Output", "Print Controller Options", "Misc."]
@@ -36,7 +36,7 @@ class ProposalsController < Sinatra::Base
     @proposal = Proposal.find(params[:id])
     @categories = ["Engine", "Install", "Paper Supply", "Output", "Print Controller Options", "Misc."]
 
-    erb :'new_line_items.html'
+    erb :'/proposals/new_line_items.html'
   end
 
   get '/proposals/:id/edit' do
@@ -48,19 +48,9 @@ class ProposalsController < Sinatra::Base
   end
 
   post '/proposals' do
-    puts "There were #{Proposal.all.count} proposals."
-    Proposal.create(account_id: Account.find(params[:account_id]).id)
-    
-    
-    params.each do |k,v|
-      if v == "on"
-      Proposal.all.last.line_items.create(product_id: k.to_i)
-      end
+    @proposal = Proposal.create(params[:proposal])
 
-      puts "Now there are #{Proposal.all.count} proposals."
-
-    end
-    redirect to "proposals/#{Proposal.all.last.id}/line_items/new"
+    redirect to "proposals/#{@proposal.id}/line_items/new"
   end
 
   post "/proposals/:id/line_items" do
@@ -68,11 +58,8 @@ class ProposalsController < Sinatra::Base
     #existing line items in the current proposal
     li = Proposal.find(params[:id]).line_items
 
-    params.each do |k,v|
+    params[:product_id].each do |k,v|
       if v == ""
-        true
-      #We want to disregard all keys that are not equal to a product id
-      elsif k == "id" || k == 
         true
       #if a line item already exists for the key being evaluated, update the quantity for line item
       elsif li.where(product_id: Product.find(k.to_i)) != []
